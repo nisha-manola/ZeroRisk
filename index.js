c9bb1a029d8d1e0bc115d9080f35d3307bf2ffe9
nisha-manola <104188809+nisha-manola@users.noreply.github.com>
2022-04-25 11:38:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,164 @@
+let randomize_array = document.getElementById("randomize_array_btn");
+let sort_btn = document.getElementById("sort_btn");
+let bars_container = document.getElementById("bars_container");
+let select_algo = document.getElementById("algo");
+let speed = document.getElementById("speed");
+let minRange = 1;
+let maxRange = 400;
+let numOfBars = 50;
+let heightFactor = 0.4;
+let speedFactor = 10; //100;
+let unsorted_array = new Array(numOfBars);
+
+speed.addEventListener("change", (e) => {
+    speedFactor = parseInt(e.target.value);
+});
+
+let algotouse = "";
+
+select_algo.addEventListener("change", function() {
+    algotouse = select_algo.value;
+});
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createRandomArray() {
+    for (let i = 0; i < numOfBars; i++) {
+        unsorted_array[i] = randomNum(minRange, maxRange);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    createRandomArray();
+    renderBars(unsorted_array);
+});
+
+function renderBars(array) {
+    for (let i = 0; i < array.length; i++) {
+        let bar = document.createElement("div");
+        bar.classList.add("bar");
+        bar.style.height = array[i] * heightFactor + "px";
+        bars_container.appendChild(bar);
+    }
+}
+
+randomize_array.addEventListener("click", function() {
+    createRandomArray();
+    bars_container.innerHTML = "";
+    renderBars(unsorted_array);
+});
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function bubbleSort(array) {
+    let bars = document.getElementsByClassName("bar");
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                for (let k = 0; k < bars.length; k++) {
+                    if (k !== j && k !== j + 1) {
+                        bars[k].style.backgroundColor = "aqua";
+                    }
+                }
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                bars[j].style.height = array[j] * heightFactor + "px";
+                bars[j].style.backgroundColor = "lightgreen";
+                //bars[j].innerText = array[j];
+                bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+                bars[j + 1].style.backgroundColor = "lightgreen";
+                //bars[j + 1].innerText = array[j + 1];
+                await sleep(speedFactor);
+            }
+        }
+        await sleep(speedFactor);
+    }
+    return array;
+}
+
+async function swap(items, leftIndex, rightIndex, bars) {
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+    bars[leftIndex].style.height = items[leftIndex] * heightFactor + "px";
+    bars[leftIndex].style.backgroundColor = "lightgreen";
+    //bars[leftIndex].innerText = items[leftIndex];
+    bars[rightIndex].style.height = items[rightIndex] * heightFactor + "px";
+    bars[rightIndex].style.backgroundColor = "lightgreen";
+    //bars[rightIndex].innerText = items[rightIndex];
+    await sleep(speedFactor);
+}
+async function partition(items, left, right) {
+    let bars = document.getElementsByClassName("bar");
+    let pivotIndex = Math.floor((right + left) / 2);
+    var pivot = items[pivotIndex]; //middle element
+    bars[pivotIndex].style.backgroundColor = "red";
+
+    for (let i = 0; i < bars.length; i++) {
+        if (i != pivotIndex) {
+            bars[i].style.backgroundColor = "aqua";
+        }
+    }
+
+    (i = left), //left pointer
+    (j = right); //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            await swap(items, i, j, bars); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+async function quickSort(items, left, right) {
+    let bars = document.getElementsByClassName("bar");
+    var index;
+    if (items.length > 1) {
+        index = await partition(items, left, right); //index returned from partition
+        if (left < index - 1) {
+            //more elements on the left side of the pivot
+            await quickSort(items, left, index - 1);
+        }
+        if (index < right) {
+            //more elements on the right side of the pivot
+            await quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+
+// sort_btn.addEventListener("click", function () {
+//   let sorted_array = quickSort(unsorted_array, 0, numOfBars - 1);
+//   console.log(sorted_array);
+// });
+
+sort_btn.addEventListener("click", function() {
+    switch (algotouse) {
+        case "bubble":
+            bubbleSort(unsorted_array);
+            break;
+        case "merge":
+            console.log(mergeSort(unsorted_array));
+            break;
+        case "quick":
+            quickSort(unsorted_array, 0, unsorted_array.length - 1);
+            break;
+        default:
+            // bubbleSort(unsorted_array);
+            quickSort(unsorted_array, 0, unsorted_array.length - 1);
+            break;
+    }
+});
